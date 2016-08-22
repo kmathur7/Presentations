1db4b4346d7f030cc1bc015db7958dea6b3e5d88
Kunal Mathur <kunal.mathur@tcs.com>
2016-08-22 09:55:56 +0530

    adding fonts

--- Restify/Examples/API/index.js
+++ Restify/Examples/API/index.js
@@ -1,10 +1,34 @@
 
 var restify = require("restify");
 
-var server = restify.createServer();
+var Logger = require('bunyan');
+var log = new Logger({
+	name: 'restifylog',
+	streams: [
+    {
+      level: 'info',
+      stream: process.stdout            // log INFO and above to stdout
+    },
+    {
+      level: 'error',
+      path: './logs/error.log'  // log ERROR and above to a file
+    }
+  ]
+});
+
+var server = restify.createServer({
+    name: 'restifyapp',
+    log: log
+});
 server.use(restify.fullResponse()).use(restify.bodyParser());
+server.pre(function(req, res, next) {
+    server.log.info({ req: req }, 'no req.log in "pre" handler');
+    next();
+});
 
 server.get('/test',function(req,res,next){
+
+	req.log.info('have "req_id" and "route" fields in route handler');
 	res.setHeader('Content-Type','application/json');
 	res.send({test:'this is a test'});
 });
@@ -18,4 +42,5 @@ server.post('/upload/test',function(req,res,next){
 
 server.listen('8080',function(){
 	console.log("server is running");
+	log.info({addr: server.address()}, 'listening');
 })
